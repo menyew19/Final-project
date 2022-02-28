bfc196d0c8f827bb925135b79b451724023b3e9b
Menyew <menye@Meaza>
2022-02-27 22:16:48 -0500

    new files added

---
+++ js/taskManager.js
@@ -0,0 +1,13 @@
+class TaskManager {
+
+    TaskManager;
+  
+    constructor(TaskManager) {
+      this.tasks 
+    }
+  
+    introduceSelf() {
+      console.log(`Hi! I'm ${this.TaskManager}`);
+    }
+  
+  }
